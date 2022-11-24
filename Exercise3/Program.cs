43a0f988d292a395575a28ec3562b3971fcf4a8a
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-11-24 16:00:30 +0700

    add variabel list empty

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -11,7 +11,45 @@ namespace Exercise3
         /*create Nodes for the circular nexted list*/
         public int rollNumber;
         public string name;
-        public Node next;
+        public Node next; 
+    }
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
+        
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        /*searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous =
+                current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*return true if the node is founds*/
+            }
+            public bool listEmpty()
+            {
+                if (LAST == null)
+                    return true;
+                else
+                    return false;
+            }
+
+            public void traverse()/*Traverse all the nodes of the list*/
+            {
+                if (listEmpty())
+                    Console.WriteLine("\nList is empty");
+                else
+                {
+                    Console.WriteLine("\nRecord in the list are:\n");
+                }
+            }
+        }
+    }
         static void Main(string[] args)
         {
         }
